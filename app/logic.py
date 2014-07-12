da69ebd090fd75b20f0fec22395be05b896c6ff0
Abhishek Gadiraju <abhishek.gadiraju@gmail.com>
2014-07-12 18:36:26 -0400

    OOP

--- app/logic.py
+++ app/logic.py
@@ -2,79 +2,79 @@ import nltk
 from nltk.corpus import wordnet as wn
 from collections import deque
 
-WORDS = set()
 
-def set_words(filename):
-  WORDS = set(line.strip() for line in open(filename))
-
-def app_logic(sentence):
-  new_word_list = [explore_syn_tree(word) for word in sentence.split(' ')]
-  return ' '.join(new_word_list)
-
-def check_in_words(word):
-  return bool(word in WORDS)
-
-def explore_syn_tree(word, pos=None):
-	if check_in_words(word):
-	  return word
-
-	synsets = None
-	posSet = False
-	if pos is None:
-		synsets = wn.synsets(word)
-	else:
-		synsets = wn.synsets(word, pos=pos)
-		posSet = True
-	for synset in synsets:
-		matching_hypernym = explore_hypernyms(synset)
-		if matching_hypernym is not None:
-			return matching_hypernym
-		matching_hyponym = explore_hyponyms(synset)
-		if matching_hyponym is not None:
-			return matching_hyponym
-	#Some handling logic
-	return "thing"
-
-def explore_hyponyms(syn):
-    count = 0
-    queue = deque()
-    visited = set()
-    queue.append(syn)
-    visited.add(syn)
-    
-    while queue:
-        current = queue.popleft()
-        if check_in_words(str(current.lemmas[0].name)):
-          return str(current.lemmas[0].name)
-        for child in current.hyponyms():
-            if child not in visited:
-                visited.add(child)
-                queue.append(child)
-                count += 1
-        if count == 10000:
-            break
-    return None
+class SimpleSentenceGenerator:
+  def __init__(filename):
+    WORDS = set(line.strip() for line in open(filename))
 
 
-def explore_hypernyms(syn):
-    count = 0
-    queue = deque()
-    visited = set()
-    queue.append(syn)
-    visited.add(syn)
-    
-    while queue:
-        current = queue.popleft()
-	if check_in_words(str(current.lemmas[0].name)):
+  def app_logic(sentence):
+    new_word_list = [explore_syn_tree(word) for word in sentence.split(' ')]
+    return ' '.join(new_word_list)
+
+  def check_in_words(word):
+    return bool(word in WORDS)
+
+  def explore_syn_tree(word, pos=None):
+	  if check_in_words(word):
+	    return word
+
+	  synsets = None
+	  posSet = False
+	  if pos is None:
+		  synsets = wn.synsets(word)
+    else:
+      synsets = wn.synsets(word, pos=pos)
+      posSet = True
+    for synset in synsets:
+      matching_hypernym = explore_hypernyms(synset)
+      if matching_hypernym is not None:
+        return matching_hypernym
+      matching_hyponym = explore_hyponyms(synset)
+      if matching_hyponym is not None:
+        return matching_hyponym
+    #Some handling logic
+    return "thing"
+
+  def explore_hyponyms(syn):
+      count = 0
+      queue = deque()
+      visited = set()
+      queue.append(syn)
+      visited.add(syn)
+      
+      while queue:
+          current = queue.popleft()
+          if check_in_words(str(current.lemmas[0].name)):
             return str(current.lemmas[0].name)
-        for child in current.hypernyms():
-            if child not in visited:
-                visited.add(child)
-                queue.append(child)
-                count += 1
-        if count == 10000:
+          for child in current.hyponyms():
+              if child not in visited:
+                  visited.add(child)
+                  queue.append(child)
+                  count += 1
+          if count == 10000:
+              break
+      return None
+
+  def explore_hypernyms(syn):
+      count = 0
+      queue = deque()
+      visited = set()
+      queue.append(syn)
+      visited.add(syn)
+      
+      while queue:
+          current = queue.popleft()
+          if check_in_words(str(current.lemmas[0].name)):
+              return str(current.lemmas[0].name)
+          for child in current.hypernyms():
+              if child not in visited:
+                  visited.add(child)
+                  queue.append(child)
+                  count += 1
+          if count == 10000:
             break
-    return None
+      return None
 
 # if __name__ == '__main__':
 #  	WORDS = generate_words('../words/1000base.txt')
