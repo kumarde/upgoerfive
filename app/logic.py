917533ae15b7ce079d7add0f5f4a4e2343db3443
debug12 <kumarde@umich.edu>
2014-07-12 15:48:55 -0400

    added foundation for logic

--- app/logic.py
+++ app/logic.py
@@ -1,2 +1,20 @@
 import nltk
 from nltk.corpus import wordnet as wn
+
+def app_logic():
+
+
+def check_in_1000_words(word):
+	pass
+
+def explore_syn_tree(word, *pos):
+	pass
+
+def explore_hyponyms(syn):
+	pass
+
+def explore_hypernyms(syn):
+	pass
+
+
+
