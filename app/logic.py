97f253f08de55bc8c23be76c711910e12d0786ee
debug12 <kumarde@umich.edu>
2014-07-12 17:32:41 -0400

    added logic for app

--- app/logic.py
+++ app/logic.py
@@ -6,11 +6,21 @@ WORDS = set()
 
 def generate_words(filename):
   return set(line.strip() for line in open(filename))
-def app_logic(): pass 
+
+def app_logic(sentence):
+	sentence_split = sentence.split(" ")
+	new_word_list = sentence_split
+	for word in sentence_split:
+			
+
+
 def check_in_words(word):
-  return word in WORDS 
+  return bool(word in WORDS)
 
 def explore_syn_tree(word, pos=None):
+	if check_in_words(word):
+	  return word
+
 	synsets = None
 	posSet = False
 	if pos is None:
@@ -30,7 +40,25 @@ def explore_syn_tree(word, pos=None):
 	return "Abhishek"
 
 def explore_hyponyms(syn):
-	return None
+    count = 0
+    queue = deque()
+    visited = set()
+    queue.append(syn)
+    visited.add(syn)
+    
+    while queue:
+        current = queue.popleft()
+	if check_in_words(str(current.lemmas[0].name)):
+            return str(current.lemmas[0].name)
+        for child in current.hyponyms():
+            if child not in visited:
+                visited.add(child)
+                queue.append(child)
+                count += 1
+        if count == 10:
+            break
+    return "Debug12"
+
 
 def explore_hypernyms(syn):
     count = 0
@@ -38,11 +66,11 @@ def explore_hypernyms(syn):
     visited = set()
     queue.append(syn)
     visited.add(syn)
+    
     while queue:
         current = queue.popleft()
-        if check_in_words(current.name):
-	    print current.name
-            return current.name
+	if check_in_words(str(current.lemmas[0].name)):
+            return str(current.lemmas[0].name)
         for child in current.hypernyms():
             if child not in visited:
                 visited.add(child)
@@ -53,4 +81,6 @@ def explore_hypernyms(syn):
     return "Debug12"
 
 if __name__ == '__main__':
+	WORDS = generate_words('../words/1000base.txt')
 	print explore_syn_tree("hi")
+	print explore_syn_tree("complicated")
